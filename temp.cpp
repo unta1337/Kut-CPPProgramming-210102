ce74c5f1b7d13b47f4e497746be7b915d48203b8
unta1337 <firstapple42@gmail.com>
2021-04-14 18:15:44 +0900

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <cmath>
+
+#define ABS(x) (x) > 0 ? (x) : -(x)
 
 int main(void)
 {
-    std::cout << "Hello, world!\n";
+    double value = 0.6117;
+    double temp = 0;
+    double error = 0.0001;
+
+    std::cout.precision(24);
+
+    int index = 1;
+    while (ABS(value - temp) > error)
+    {
+        temp += 1 / pow(2, index);
+        if (temp > value)
+        {
+            temp -= 1 / pow(2, index);
+            std::cout << index << ", " << 0 << ", " << temp << "\n";
+            index++;
+            continue;
+        }
+
+        std::cout << index << ", " << 1 << ", " << temp << "\n";
+        index++;
+    }
 
     return 0;
 }
