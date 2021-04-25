704aa2e1eff3896d4920a3e552852b9faee89796
unta1337 <firstapple42@gmail.com>
2021-04-25 14:54:24 +0900

    Lab08 2

--- L8/p7.cpp
+++ L8/p7.cpp
@@ -1,1 +1,32 @@
 // Triangle.
+
+#include <iostream>
+
+class Triangle
+{
+    float base;
+    float height;
+    float area;
+
+    public:
+        Triangle(float base_, float height_)
+        {
+            base = base_;
+            height = height_;
+            area = base * height * 0.5f;
+        }
+
+        float getArea()
+        {
+            return area;
+        }
+};
+
+int main(void)
+{
+    Triangle t(3.14, 2.71);
+
+    std::cout << t.getArea();
+
+    return 0;
+}
--- L8/p8.cpp
+++ L8/p8.cpp
@@ -1,1 +1,69 @@
 // Bank account.
+
+#include <iostream>
+
+class BankAccount
+{
+    int accountNumber;
+    std::string owner;
+    int balance;
+
+    public:
+        BankAccount(int accountNumber_, std::string owner_, int balance_)
+        {
+            accountNumber = accountNumber_;
+            owner = owner_;
+            balance = balance_;
+        }
+
+        // Use pointer to mod original object.
+        int transfer(int amount, BankAccount *otherAccount)
+        {
+            withdraw(amount);
+            otherAccount->deposit(amount);
+
+            return amount;
+        }
+
+        void setBalance(int amount)
+        {
+            balance = amount;
+        }
+
+        int getBalance()
+        {
+            return balance;
+        }
+
+        void deposit(int amount)
+        {
+            balance += amount;
+        }
+
+        void withdraw(int amount)
+        {
+            balance -= amount;
+        }
+
+        void print()
+        {
+            std::cout << "[" << owner << "] Your balance: " << balance << "\n";
+        }
+};
+
+int main(void)
+{
+    BankAccount b1(1, "name1", 1000);
+    BankAccount b2(1, "name2", 0);
+
+    b1.print();
+    b2.print();
+
+    b1.transfer(1000, &b2);
+    std::cout << "\n";
+
+    b1.print();
+    b2.print();
+
+    return 0;
+}
