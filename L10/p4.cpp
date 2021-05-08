9c75a8e464f0a790000aba92e62e0150dc1c6490
unta1337 <firstapple42@gmail.com>
2021-05-08 15:43:48 +0900

    Lab10 finished

--- L10/p4.cpp
+++ L10/p4.cpp
@@ -1,1 +1,32 @@
 // Foods.
+
+#include <iostream>
+
+class Food
+{
+public:
+    int kcal;
+    int price;
+    int weight;
+
+    Food(int kcal_, int price_, int weight_) : kcal(kcal_), price(price_), weight(weight_) { }
+};
+
+class Melon : public Food
+{
+public:
+    std::string farm;
+
+    Melon(int kcal_, int price_, int weight_, std::string farm_) : Food(kcal_, price_, weight_), farm(farm_) { }
+};
+
+int main(void)
+{
+    Melon m(500, 20000, 2, "farm name");
+    std::cout << "kacl: " << m.kcal << "\n";
+    std::cout << "price: " << m.price << "\n";
+    std::cout << "weight: " << m.weight << "\n";
+    std::cout << "farm: " << m.farm << "\n";
+
+    return 0;
+}
